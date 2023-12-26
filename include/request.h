ff915901a24961896aab338dfc1d5efc73ed60f2
Kirill Guryanov <93494061+Korako2@users.noreply.github.com>
2023-12-26 10:56:21 +0300

    add functionality (#1)
    * feat: add main
    
    * feat: add parser
    
    * fix: bug in parser
    
    * feat: add view
    
    * feat: add  request
    
    * fix: small changes
    
    * feat: add read_word in parser
    
    * feat: add read_attributes in parser
    
    * fix:  bug in print_attribute

---
+++ include/request.h
@@ -0,0 +1,22 @@
+#ifndef GRAPH_QL_REQUEST_H
+#define GRAPH_QL_REQUEST_H
+
+#define MAX_REQUEST_SIZE 1024
+#define MAX_STRING_SIZE 64
+#define TAB "    "
+
+struct attribute {
+    char *left;
+    char *right;
+    char *condition;
+    struct attribute *next_attribute;
+    char *combined_condition;
+};
+
+struct request {
+    char *operation;
+    char *parent_id;
+    struct attribute *attributes;
+};
+
+#endif //GRAPH_QL_REQUEST_H
